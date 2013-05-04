bf0d501d386a519ab37eb9a5bb81b705e5115428
Ben <zygoth2@gmail.com>
2013-05-04 13:46:25 -0600

    test commit

---
+++ BZRFlag AI/TankAI.cpp
@@ -0,0 +1,18 @@
+/*
+ * TankAI.cpp
+ *
+ *  Created on: May 4, 2013
+ *      Author: Ben
+ */
+
+#include "TankAI.h"
+
+TankAI::TankAI() {
+	// TODO Auto-generated constructor stub
+
+}
+
+TankAI::~TankAI() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ BZRFlag AI/TankAI.h
@@ -0,0 +1,17 @@
+/*
+ * TankAI.h
+ *
+ *  Created on: May 4, 2013
+ *      Author: Ben
+ */
+
+#ifndef TANKAI_H_
+#define TANKAI_H_
+
+class TankAI {
+public:
+	TankAI();
+	virtual ~TankAI();
+};
+
+#endif /* TANKAI_H_ */

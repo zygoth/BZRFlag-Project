53ee15098b473b9a35f44207a97f4685072194a7
zygoth <zygoth2@gmail.com>
2013-05-13 16:17:33 -0600

    ???gffffgffg

--- BZRFlag AI/Lab1_Main.cpp
+++ BZRFlag AI/Lab1_Main.cpp
@@ -8,22 +8,22 @@
 #include "BZRFlagGame.h"
 using namespace std;
 
-const char *kDefaultServerName = "localhost";
+char *kDefaultServerName = "localhost";
 const int kDefaultServerPort = 4000;
 
-int main(int argc, char *argv[]) 
+void doAgentMain(int argc, char *argv[])
 {
-    const char* pcHost = kDefaultServerName;
+    char* pcHost = kDefaultServerName;
     int nPort = kDefaultServerPort;
-    
-    if (argc >= 2)
-    {
-        pcHost = argv[1];
-    }
     
     if (argc >= 3)
     {
-        nPort = atoi(argv[2]);
+        pcHost = argv[2];
+    }
+    
+    if (argc >= 4)
+    {
+        nPort = atoi(argv[3]);
     }
     
     string hostName(pcHost);    
@@ -32,7 +32,55 @@ int main(int argc, char *argv[])
     game->playGame(hostName, nPort);
 
     delete game;
-    return 0;
+    
+    exit(0);
 }
+
+void doPotentialFieldPrinterMain(int argc, char *argv[])
+{
+    
+}
+
+void doSearchMain(int argc, char *argv[])
+{
+    
+}
+
+void printMainErrorMessage()
+{
+        cout << "Please tell me which program to run.  \"agent HOSTNAME PORTNUMBER\",";
+        cout << "\"potentialfieldprinter\", or \"search ALGORITHM\"\n";
+}
+
+int main(int argc, char *argv[]) 
+{
+    if(argc < 1)
+    {
+        printMainErrorMessage();
+        exit(1);
+    }
+    
+    if(strcmp(argv[1], "agent") == 0)
+    {
+        doAgentMain(argc, argv);
+    }
+    else 
+    if(strcmp(argv[1], "potentialfieldprinter") == 0)
+    {
+            doPotentialFieldPrinterMain(argc, argv);
+    }    
+    else 
+    if(strcmp(argv[1], "search") == 0)
+    {
+        doSearchMain(argc, argv);
+    }
+    else
+    {
+        printMainErrorMessage();
+        exit(1);
+    }
+}
+
+
 
 #endif	/* MAIN_H */

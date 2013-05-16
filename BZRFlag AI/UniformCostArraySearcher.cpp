88cd4a3f47cd442c60767aded6a69a2539977c52
Chris Elliott <jets_million@yahoo.com>
2013-05-15 20:09:00 -0600

    Update UC Search

--- BZRFlag AI/UniformCostArraySearcher.cpp
+++ BZRFlag AI/UniformCostArraySearcher.cpp
@@ -15,8 +15,10 @@ UniformCostArraySearcher::UniformCostArraySearcher()
     objectGrid = NULL;
     width = 0; 
     height = 0;
+    printCounter = 0;
     aStar = false;
     penaltyMode = false;
+    printer = new GNUPrinter();
 }
 
 UniformCostArraySearcher::UniformCostArraySearcher(const UniformCostArraySearcher& orig)
@@ -45,6 +47,7 @@ bool UniformCostArraySearcher::search(bool* occgrid, int gridWidth, int gridHeig
     int tempX = targetPosition.x;
     int tempY = targetPosition.y;
     
+    grid[tempNode->y*width + tempNode->x] = 1;
     pathOptions.push(tempNode);
     tree = tempNode;
     
@@ -55,20 +58,33 @@ bool UniformCostArraySearcher::search(bool* occgrid, int gridWidth, int gridHeig
         if(tempNode->x == tempX && tempNode->y == tempY)
             break;
         
-        grid[tempNode->y*width + tempNode->x] = 1;
         addchildren(tempNode);
         
     }
     
-    if(tempNode->x != tempX && tempNode->y != tempY)
+    if(tempNode->x != tempX && tempNode->y != tempY){
+        printer->outputToFile("UCSearch");
         return false;
-    
-    while(tempNode->parent != NULL){
-        Point pathPoint(tempNode->x, tempNode->y); 
-        path.push_back(pathPoint);
-        tempNode = tempNode->parent;
     }
     
+    node_uc* parent = tempNode->parent;
+    printer->insertClearGraph();
+    while(parent != NULL){
+        int x1 = parent->x;
+        int y1 = parent->y;
+        int x2 = tempNode->x;
+        int y2 = tempNode->y;
+        
+        printer->insertLine(x1, y1, x2, y2, true);
+        
+        Point pathPoint(x2, y2); 
+        path.push_back(pathPoint);
+        tempNode = parent;
+        parent = parent->parent;
+    }
+    
+    printer->insertSquare(tempX, tempY);
+    printer->outputToFile("UCSearch");
     return true;
 }
 
@@ -148,6 +164,7 @@ bool UniformCostArraySearcher::isNextToObject(int x,int y)
 
 void UniformCostArraySearcher::addchildren(node_uc* parent)
 {
+    double currentCost;
     int x = parent->x;
     int y = parent->y;
     double amount = parent->cost;
@@ -157,34 +174,85 @@ void UniformCostArraySearcher::addchildren(node_uc* parent)
         if(y>0){
             // check lower-left
             if(grid[(y-1)*width + (x-1)] == false){
-                child = newNode(amount + 1.0, x-1, y-1, parent);
+                currentCost = amount + 1.41421;
+                child = newNode(currentCost, x-1, y-1, parent);
+                printer->insertLine(x, y, x-1, y-1, true);
+                grid[(y-1)*width + (x-1)] = 1;
                 
                 pathOptions.push(child);
             }
         }
-        
-        // check left
-        if(grid[(y)*width + (x-1)] == false){
-                child = newNode(amount + 1.0, x-1, y, parent);
-                
-                pathOptions.push(child);
-            }
         
         if(y<height-1){
             // check upper-left
             if(grid[(y+1)*width + (x-1)] == false){
-                child = newNode(amount + 1.0, x-1, y+1, parent);
+                currentCost = amount + 1.41421;
+                child = newNode(currentCost, x-1, y+1, parent);
+                printer->insertLine(x, y, x-1, y+1, true);
+                grid[(y+1)*width + (x-1)] = 1;
                 
                 pathOptions.push(child);
             }
         }
+        // check left
+        if(grid[(y)*width + (x-1)] == false){
+                currentCost = amount + 1.0;
+                child = newNode(currentCost, x-1, y, parent);
+                printer->insertLine(x, y, x-1, y, true);
+                grid[(y)*width + (x-1)] = 1;
+                
+                pathOptions.push(child);
+            }
+        
+        
+    }
+    
+    // right
+    if(x<width-1){
+        if(y>0){
+            // check lower-right
+            if(grid[(y-1)*width + (x+1)] == false){
+                currentCost = amount + 1.41421;
+                child = newNode(currentCost, x+1, y-1, parent);
+                printer->insertLine(x, y, x+1, y-1, true);
+                grid[(y-1)*width + (x+1)] = 1;
+                
+                pathOptions.push(child);
+            }
+        }
+        
+        if(y<height-1){
+            // check upper-right
+            if(grid[(y+1)*width + (x+1)] == false){
+                currentCost = amount + 1.41421;
+                child = newNode(currentCost, x+1, y+1, parent);
+                printer->insertLine(x, y, x+1, y+1, true);
+                grid[(y+1)*width + (x+1)] = 1;
+                
+                pathOptions.push(child);
+            }
+        }
+        
+        // check right
+        if(grid[(y)*width + (x+1)] == false){
+                currentCost = amount + 1.0;
+                child = newNode(currentCost, x+1, y, parent);
+                printer->insertLine(x, y, x+1, y, true);
+                grid[(y)*width + (x+1)] = 1;
+                
+                pathOptions.push(child);
+            }
+
     }
     
     // middle
     if(y>0){
         // check down
         if(grid[(y-1)*width + (x)] == false){
-                child = newNode(amount + 1.0, x, y-1, parent);
+                currentCost = amount + 1.0;
+                child = newNode(currentCost, x, y-1, parent);
+                printer->insertLine(x, y, x, y-1, true);
+                grid[(y-1)*width + (x)] = 1;
                 
                 pathOptions.push(child);
             }
@@ -193,38 +261,18 @@ void UniformCostArraySearcher::addchildren(node_uc* parent)
     if(y<height-1){
         // check up
         if(grid[(y+1)*width + (x)] == false){
-                child = newNode(amount + 1.0, x, y+1, parent);
+                currentCost = amount + 1.0;
+                child = newNode(currentCost, x, y+1, parent);
+                printer->insertLine(x, y, x, y+1, true);
+                grid[(y+1)*width + (x)] = 1;
                 
                 pathOptions.push(child);
             }
     }
     
-    // right
-    if(x<width-1){
-        if(y<0){
-            // check lower-right
-            if(grid[(y-1)*width + (x+1)] == false){
-                child = newNode(amount + 1.0, x+1, y-1, parent);
-                
-                pathOptions.push(child);
-            }
-        }
-        
-        // check right
-        if(grid[(y)*width + (x+1)] == false){
-                child = newNode(amount + 1.0, x+1, y, parent);
-                
-                pathOptions.push(child);
-            }
-        
-        if(y>height-1){
-            // check upper-right
-            if(grid[(y+1)*width + (x+1)] == false){
-                child = newNode(amount + 1.0, x+1, y+1, parent);
-                
-                pathOptions.push(child);
-            }
-        }
+    printCounter++;
+    if(printCounter > 1000){
+        printCounter = 0;
+        printer->insertPause(.000001);
     }
-    
 }

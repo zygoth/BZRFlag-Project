b21741da663b2e6a0d75e59397cefa11ea5349b8
zygoth <zygoth2@gmail.com>
2013-05-25 11:47:11 -0600

    Getting ready for search lab passoff

--- BZRFlag AI/TargetPointCalculator.cpp
+++ BZRFlag AI/TargetPointCalculator.cpp
@@ -8,7 +8,14 @@
 #include "TargetPointCalculator.h"
 #include <iostream>
 #include <unistd.h>
+#include <cstdlib>
+#include "SearchTools.h"
 
+/**
+ * tankX and tankY should be in matrix coordinates, not cartesian coordinates.
+ * @param tankX
+ * @param tankY
+ */
 TargetPointCalculator::TargetPointCalculator(int tankX, int tankY) : previousTarget(tankX, tankY)
 {
     
@@ -24,10 +31,123 @@ Point TargetPointCalculator::getTarget(int tankX, int tankY, settledGrid_t settl
         return previousTarget;
     }
     
-    //  Do the basic Search
+    //  Do the basic Search, then if that fails do the more complex one
+    vector<Point> targets;
+    
+    basicPointSearch(tankX, tankY, settledGrid, &targets);
+    
+    if(!targets.empty())
+    {
+        return targets[0];
+    }
+    
+    advancedPointSearch(tankX, tankY, settledGrid, &targets);
+    
+    if(!targets.empty())
+    {
+        return targets[0];
+    }
+    else
+    {
+        return previousTarget;
+    }
 }
 
-void TargetPointCalculator::basicPointSearch(vector<Point>* nearbyPoints)
+/**
+ * Checks 8 different Points near the tank to look for unsettled ones.
+ * 
+ * @param tankX
+ * @param tankY
+ * @param settledGrid
+ * @param nearbyPoints
+ */
+void TargetPointCalculator::basicPointSearch(int tankX, int tankY,
+                            settledGrid_t settledGrid, vector<Point>* targetPoints)
 {
+    const int NUMPOINTS = 8;
     
+    Point testedPoints[NUMPOINTS];
+    
+    testedPoints[0].x = tankX - 50;
+    testedPoints[0].y = tankY;
+    
+    testedPoints[1].x = tankX;
+    testedPoints[1].y = tankY + 50;
+    
+    testedPoints[2].x = tankX + 50;
+    testedPoints[2].y = tankY;
+    
+    testedPoints[3].x = tankX;
+    testedPoints[3].y = tankY - 50;
+    
+    
+    testedPoints[4].x = tankX - 100;
+    testedPoints[4].y = tankY;
+    
+    testedPoints[5].x = tankX;
+    testedPoints[5].y = tankY + 100;
+    
+    testedPoints[6].x = tankX + 100;
+    testedPoints[6].y = tankY;
+    
+    testedPoints[7].x = tankX;
+    testedPoints[7].y = tankY - 100;
+    
+    
+    for(int i = 0; i < NUMPOINTS; i++)
+    {        
+        if(isValidTarget(testedPoints[i], settledGrid))
+        {
+             targetPoints->push_back(testedPoints[i]);
+        }
+    }
+}
+
+void TargetPointCalculator::advancedPointSearch(int tankX, int tankY, 
+                        settledGrid_t settledGrid, vector<Point>* nearbyPoints)
+{
+    const int GRANULARITY = 50;
+    int noise = rand() % GRANULARITY;
+    Point currentPoint(0,0);
+    
+    for(int i = 0; i < settledGrid.width; i+= GRANULARITY - noise)
+    {
+        for(int j = 0; j < settledGrid.height; j+= GRANULARITY - noise)
+        {
+            currentPoint.x = i;
+            currentPoint.y = j;
+            
+            if(isValidTarget(currentPoint, settledGrid))
+            {
+                nearbyPoints->push_back(currentPoint);
+            }
+        }
+    }
+    
+}
+
+/**
+ * Returns true if the point exists on the grid and is not already settled.
+ * 
+ * @param p
+ * @param settledGrid
+ * @return 
+ */
+bool TargetPointCalculator::isValidTarget(Point p, settledGrid_t settledGrid)
+{
+    double settledValue;
+    
+    if (!SearchTools::isValidPoint(settledGrid.width, settledGrid.height, p))
+    {
+        return false;
+    }
+
+    settledValue = settledGrid.grid[p.x][p.y];
+
+    if (settledValue == 1 || settledValue == 0)
+    {
+        return false;
+    }
+    
+    return true;
 }

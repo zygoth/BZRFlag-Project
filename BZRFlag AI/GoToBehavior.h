32edac0c9d10848009ad2bd76aee74e6eee0d91d
zygoth <zygoth2@gmail.com>
2013-06-12 15:00:16 -0600

    Behavior Tank Classes

--- BZRFlag AI/GoToBehavior.h
+++ BZRFlag AI/GoToBehavior.h
@@ -11,18 +11,23 @@
 #include "Behavior.h"
 #include "PDController.h"
 #include "UniformCostArraySearcher.h"
+#include "Point.h"
 
 class GoToBehavior : public Behavior
 {
 public:
     GoToBehavior(BZRC* server, int tankNumber, TeamColor myColor,
-                vector<TankTargeter>* enemies, UniformCostArraySearcher* pathFinder);
-    GoToBehavior(const GoToBehavior& orig, UniformCostArraySearcher* pathFinder);
+                vector<TankTargeter>* enemies, UniformCostArraySearcher* pathFinder,
+                Point targetPoint);
+    GoToBehavior(const Behavior& orig, UniformCostArraySearcher* pathFinder, Point targetPoint);
     virtual void doMove();
     virtual ~GoToBehavior();
     
 private:
+    Point targetPoint;
     PDController pdController;
+    
+    // this is owned by the Commander and used by other TankAIs--don't delete it!
     UniformCostArraySearcher* pathFinder;
 };
 
